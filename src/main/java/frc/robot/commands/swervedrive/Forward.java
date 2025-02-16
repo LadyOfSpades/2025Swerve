ac50764fffdc1d530da1720bdea3255bce9dff0e
LadyOfSpades <haliekarre@gmail.com>
2025-02-16 13:05:57 -0600

    Init

---
+++ src/main/java/frc/robot/commands/swervedrive/Forward.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.swervedrive;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+
+public class Forward extends Command {
+    SwerveSubsystem swerveSubsystem;
+    public Forward(SwerveSubsystem subsystem) {
+        swerveSubsystem = subsystem;
+    }
+    public void execute() {
+        swerveSubsystem.drive(new Translation2d(0.5, 0), 0, false);
+    }
+}
